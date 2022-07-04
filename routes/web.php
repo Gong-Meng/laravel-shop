45452f865c25c1a0446bb85ffa9dc93d159a2259
Gong Meng <18588264128@163.com>
2022-07-04 14:39:03 +0800

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -48,10 +48,11 @@ Route::group(['middleware' => ['auth']], function() {
     Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
 
     Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
-
     Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
     Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 });
 
 Route::redirect('/', '/products')->name('root');

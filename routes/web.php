a347ec975be9959d101e91961600cad6a95d5ed4
Gong Meng <18588264128@163.com>
2022-07-07 18:21:59 +0800

    前台分期付款详情页

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,7 @@ Route::group(['middleware' => ['auth']], function() {
     //分期支付
     Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
     Route::get('installments', 'InstallmentsController@index')->name('installments.index');
+    Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
 });
 
 Route::redirect('/', '/products')->name('root');

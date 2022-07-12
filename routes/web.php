ab01d4f526875dee2c9511d2e0c736f735e02fe3
Gong Meng <18588264128@163.com>
2022-07-12 15:40:52 +0800

    随机拒绝

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 //Auth::routes(['verify' => true]);
 Auth::routes();
 
-Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
+Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store')->middleware('random_drop:80');
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 //Route::group(['middleware' => ['auth', 'verified']], function() {
